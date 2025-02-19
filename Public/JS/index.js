d2a83c3e5701273bd511d7fabca8531b83dd9531
ashish-saha <rubelsaha1@gmail.com>
2025-02-19 23:41:00 +0600

    add serarch bar js code in my style from home

--- Public/JS/index.js
+++ Public/JS/index.js
@@ -1,2 +1,26 @@
-// alert('I am Aleart')
-// console.log('Hell');
+
+const searchBar = document.querySelector('.searchBar');
+const searchClose = document.querySelector('#searchClose');
+const searchBtn = document.querySelector('.searchBtn');
+
+/**
+ * Search Bar visiable 
+ */
+searchBtn.addEventListener('click', function(){
+    searchBar.style.visibility = "visible";
+    searchBar.classList.add('open')
+    this.setAttribute("aria-expanded", "true")
+    
+    
+})
+
+/**
+ * Search Bar Hide 
+ */
+searchClose.addEventListener('click', function(){
+    searchBar.style.visibility = "hidden";
+    searchBar.classList.remove('open')
+    searchBtn.setAttribute("aria-expanded", "false")
+    
+    
+})
